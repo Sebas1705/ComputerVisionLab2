5d1d3dc60f57fc680f200004abad3c7dd91c1006
Alvaro Manzanero <a.manzanerog@alumnos.urjc.es>
2024-05-26 20:39:30 +0200

    version entrega practica

--- proyect/src/main.py
+++ proyect/src/main.py
@@ -3,8 +3,8 @@ from Scripts.Tests.Ex3_Test import test as ex_3_test
 from Scripts.Tests.Ex4_Test import test as ex_4_test
 #Main function of proyect:
 def main() -> None:
-    #exs_1_2_test(ex1=None,classifier_name="All")
-    # ex_3_test()
+    exs_1_2_test(ex1=None,classifier_name="All")
+    ex_3_test()
     ex_4_test()
     
 if __name__ == '__main__':

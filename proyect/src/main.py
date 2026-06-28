77eff18f900c145684ea2a055303dbe405ade34f
Sebastián Ramiro Entrerrios García <100527906+Sebas1705@users.noreply.github.com>
2026-06-28 14:39:38 +0200

    i18n: translate Spanish comment in main.py

--- proyect/src/main.py
+++ proyect/src/main.py
@@ -1,7 +1,7 @@
 from Scripts.Tests.Ex1_and_Ex2_Test import test as exs_1_2_test
 from Scripts.Tests.Ex3_Test import test as ex_3_test
 from Scripts.Tests.Ex4_Test import test as ex_4_test
-#Main function of proyect:
+#Main function:
 def main() -> None:
     exs_1_2_test(ex1=None,classifier_name="All")
     ex_3_test()
